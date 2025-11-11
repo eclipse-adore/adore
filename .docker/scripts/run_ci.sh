0371bfb969473b953afa2515652afd8dba539083
Marko Mizdrak <marko.mizdrak@dlr.de>
2025-11-11 16:55:25 +0100

    let running ci docker from github
    Signed-off-by: Marko Mizdrak <marko.mizdrak@dlr.de>

--- .docker/scripts/run_ci.sh
+++ .docker/scripts/run_ci.sh
@@ -7,7 +7,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${SCRIPT_DIR}/common.sh"
 
-require_host "CI convenience wrapper should be run on the host."
-
 "${SCRIPT_DIR}/run_tests.sh"
 "${SCRIPT_DIR}/run_docs.sh"
