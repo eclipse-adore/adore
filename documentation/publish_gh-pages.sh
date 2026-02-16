6162a820ec7c54866c40f65c68a4426ab8942334
MikkelDLR <150240396+MikkelDLR@users.noreply.github.com>
2026-02-16 13:44:56 +0100

    Fixed publish_gh-pages (#77)
    Description
    made publish_gh-pages executable, added publish as a just command, and
    modified publish_gh-pages
    
    Fixes #(issue-number)
    
    #75 
    
    ## Type of Change
    
    Please delete options that are not relevant:
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [x] Documentation update (adds or updates documentation)
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code.
    - [x] My last commit was made with the `--signoff` flag as required by
    the Eclipse Foundation.
    
    ---------
    
    Signed-off-by: Mikkel Skov Maarssoe <mikkel.maarssoe@dlr.de>

--- documentation/publish_gh-pages.sh
+++ documentation/publish_gh-pages.sh
@@ -46,9 +46,9 @@ if [[ $PROHIBITED_REMOTES == *"$git_url"* ]]; then
 fi
 
 read -p "Should the remote publish branch: ${PUBLISH_BRANCH} be deleted? (y/n): " answer
-if ! [[ $answer == [Yy] ]]; then
-    git push ${PUBLISH_REMOTE} --delete "${PUBLISH_BRANCH}"
-    git branch -d "${PUBLISH_BRANCH}"
+if [[ $answer == [Yy]* ]]; then
+    git push ${PUBLISH_REMOTE} --delete "${PUBLISH_BRANCH}" || true
+    git branch -d "${PUBLISH_BRANCH}" || true
 fi
 
 cd "${CLONE_DIRECTORY}"
@@ -58,10 +58,32 @@ git clone --depth 1 --single-branch -b "${DOCUMENTATION_SOURCE_BRANCH}" "${git_u
 cd ${CLONE_DIRECTORY}/adore
 git checkout --orphan "${PUBLISH_BRANCH}"
 
+# Build documentation block
 {
-cd ${CLONE_DIRECTORY}/adore/documentation
-make build
-}
+    cd ${CLONE_DIRECTORY}/adore/documentation
+    
+    echo "Building documentation in ${PWD}..."
+    
+    # Justfile logic: docs_build_mkdocs
+    mkdir -p mkdocs/docs
+    rm -rf mkdocs/docs/generated mkdocs/site
+    
+    cp -r technical_reference_manual mkdocs/docs/technical_reference_manual
+    
+    echo "Generaring docs with gen_docs.py..."
+    python3 mkdocs/gen_docs.py
+    
+    echo "Running mkdocs build..."
+    (cd mkdocs && mkdocs build)
+
+    # Justfile logic: docs_build
+    rm -rf docs
+    mkdir -p docs
+    cp -r mkdocs/site docs/mkdocs
+    cp -r mkdocs/img docs/mkdocs
+    cp -r mkdocs/stylesheets docs/mkdocs
+    cp -r mkdocs/overrides docs/mkdocs
+} || exiterr "Documentation build failed"
 
 cd ${CLONE_DIRECTORY}/adore
 git reset
@@ -72,6 +94,13 @@ if [[ "${PUBLISH_DIRECTORY}" == "." ]]; then
     cp -r ${CLONE_DIRECTORY}/adore/documentation/docs/* .
     git add . --force
 else
+    # Ensure parent directory exists if nested
+    mkdir -p $(dirname "${PUBLISH_DIRECTORY}")
+    
+    # If PUBLISH_DIRECTORY exists, we might want to clear it or just overwrite.
+    # The original script did mv, which implies the target should probably be replaced or filled.
+    # Since we are in a fresh orphan branch or clean state (mostly), we can just move.
+    rm -rf "${PUBLISH_DIRECTORY}"
     mv ${CLONE_DIRECTORY}/adore/documentation/docs "${PUBLISH_DIRECTORY}"
     git add "${PUBLISH_DIRECTORY}" --force
 fi
